4b4d3296ed1aca9b5754474d3226ed1df201432b
Rafiizs <123raffimhmd@gmail.com>
2024-09-25 16:15:34 +0800

    Pertemuan 4

---
+++ Pertemuan_4/2409106095_MuhammadRafiiZaidanSakaria_C1'24_Perulangan.py
@@ -0,0 +1,94 @@
+# ulang = 10
+# for i in range (ulang):
+#     print("halo")
+
+# simpan = [12, "udin petot", 14.5, True, 'A', "102"]
+# for i in simpan:
+#     print(i)
+# print (simpan)
+# print(simpan[4])
+
+# simpan = [12, "udin petot", 14.5]
+# for i in simpan:
+#     print(i)
+
+# simpan = [12, "udin petot", 14.5, True, 'A', "102"]
+# for i in simpan [2 : 5]:
+#     print(i)
+
+# for i in range(1, 4):
+#     for j in range(1, 4):
+#         print(f"{i} x {j} = {i * j}")
+#     print()
+
+# makanan = ["mie", "sop", "bakso"]
+# minuman = ["es teh", "air putih", "es jeruk"]
+
+# for i in makanan:
+#     for j in minuman:
+#         print(f"{i} & {j}")
+
+# print("Menu Rumah Makan Informatika 24 : ")
+# print("----------------------------------")
+# simpan = ["Nasi Goreng", "Mie Goreng", "Mie Ayam", "Bakso", "Soto"]
+# for i in simpan:
+#     print(i)
+
+# print("Menu Rumah Makan Informatika 24 : ")
+# print("----------------------------------")
+# simpan = ["Nasi Goreng", "Mie Goreng", "Mie Ayam", "Bakso", "Soto"]
+# for i, menu in enumerate(simpan, start=1):
+#     print(f"{i}.{menu}")
+
+# print("Menu Rumah Makan Informatika 24 : ")
+# print("----------------------------------")
+# simpan = ["Nasi Goreng", "Mie Goreng", "Mie Ayam", "Bakso", "Soto"]
+# for i in range(len(simpan)):
+#     print(f"{i+1}. {simpan[1]}")
+
+# jawab = 'ya'
+# hitung = 0
+# while(jawab == 'ya'):
+#     hitung += 1
+#     jawab = input("Ulang lagi tidak? ")
+# print(f"Total perulangan: {hitung}")
+
+# jawab = 'ya'
+# hitung = 0
+# while(True):
+#     hitung += 1
+#     jawab = input("Ulang lagi tidak? ")
+#     if jawab == "ga" or jawab == "engga" :
+#         print(f"Total perulangan: {hitung}")
+#     else :
+#         break
+
+# hitung = 0
+# while True:
+#     hitung += 1
+#     ulang = input("Masih Ingin Mengulang? ")
+#     if ulang == "tidak" or ulang =="Tidak":
+#         break
+# print(f"Total Perulangan: {hitung}")
+
+# jawab = 'ya'
+# hitung = 0
+# while(True):
+#     hitung += 1
+#     jawab = input("Ulang lagi tidak? ")
+#     if jawab == "ga" or jawab == "engga" :
+#           break
+
+# print("Daftar bilangan ganjil dari 1-10")
+# for i in range(10):
+#     if i % 2 == 0:
+#         continue
+#     print(i)
+
+# hitung = 0
+# while True:
+#     hitung += 1
+#     ulang = input("Masih Ingin Lanjut? ")
+#     if ulang == "y" or ulang =="y":
+#         print("Oke Kita Lanjut")
+#         continue
