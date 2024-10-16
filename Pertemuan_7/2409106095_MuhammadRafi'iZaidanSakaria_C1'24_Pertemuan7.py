ab9671f2287c78db36c85e8ff25afb20d2980b68
Rafiizs <123raffimhmd@gmail.com>
2024-10-16 16:26:32 +0800

    Pertemuan7

---
+++ Pertemuan_7/2409106095_MuhammadRafi'iZaidanSakaria_C1'24_Pertemuan7.py
@@ -0,0 +1,155 @@
+# def salam():
+#     print("halo semua")
+
+# salam()
+
+# def salam():
+#     print("halo semua")
+
+# pilihan = 0
+
+# if pilihan == 1:
+#     salam()
+# elif pilihan == 2:
+#     salam()
+# elif pilihan == 3:
+#     salam()
+# elif pilihan == 4:
+#     salam()
+
+# def teks():
+#     print("halo semua")
+
+# pilihan = 0
+
+# if pilihan == 1:
+#     teks()
+# elif pilihan == 2:
+#     teks()
+# elif pilihan == 3:
+#     teks()
+# elif pilihan == 4:
+#     teks()
+# elif pilihan == 5:
+
+
+# def salam():
+#     print("halo selamat sore")
+
+# salam()
+
+# def penjumlahan():
+#     hasil=7+3
+#     print (hasil)
+
+# penjumlahan()
+
+# def penjumlahan():
+#     hasil=1000000+20
+#     print(hasil)
+
+# penjumlahan()
+
+# def penjumlahan(a,b,c):
+#     hasil = a+b+c
+#     print(hasil)
+
+# penjumlahan(3,5,8)
+
+# a = int(input ("bilangan pertama: "))
+# b = int(input ("bilangan kedua: "))
+# c = int(input ("bilangan ketida: "))
+
+# def penjumlahan(a,b,c):
+#     hasil = a+b+c
+#     print(hasil)
+
+# penjumlahan(a,b,c)
+
+# def perkalian(a,b):
+#     hasil = a*b
+#     print(hasil)
+
+# perkalian(5,3)
+
+# def luas_persegi(sisi):
+#     luas = sisi*sisi
+#     print(f"luas persegi: {luas}")
+
+# sisi = int(input("sisi persegi: "))
+
+# luas_persegi(sisi)
+
+# def volume_persegi(sisi):
+#     luas = sisi*sisi*sisi
+#     print(f"volume persegi: {luas}")
+
+# sisi = int(input("sisi persegi: "))
+
+# volume_persegi(sisi)
+
+# def kali(a,b):
+#     hasil = a*b
+#     return hasil
+
+# print(kali(5,5))
+
+# # rumus: sisi x sisi
+# def luas_persegi(sisi):
+#     luas = sisi * sisi
+#     return luas
+# # rumus: sisi x sisi x sisi
+# def volume_persegi(sisi):
+#     volume = luas_persegi(sisi) * sisi
+#     print ("Volume Persegi = ", volume)
+# # pemanggilan Fungsi
+# volume_persegi(6)
+
+# a = int(input("Masukkan Angka: "))
+# def penjumlahan (a):
+#     hasil = a+a
+#     return hasil
+
+# def perkalian (a):
+#     hasil = penjumlahan(a)*a
+#     print(f"hasil = {hasil}")
+
+# perkalian(a)
+
+# a=1
+# b=2
+
+# def penjumlahan(a,b):
+#     a= 5
+#     b= 4
+#     hasil = a+b
+#     print(hasil)
+
+# penjumlahan(a,b)
+
+# fungsi untuk menampilkan menu
+# def show_menu():
+#     print ("\n")
+#     print ("----------- MENU---------- ")
+#     print ("[1] Show Data")
+#     print ("[2] Insert Data")
+#     print ("[3] Edit Data")
+#     print ("[4] Delete Data")
+#     print ("[5] Exit")
+#     menu = input("PILIH MENU> ")
+#     print ("\n")
+#     if menu == "1":
+#         show_data()
+#     elif menu == "2":
+#         insert_data()
+#     elif menu == "3":
+#         edit_data()
+#     elif menu == "4":
+#         delete_data()
+#     elif menu == "5":
+#         exit()
+#     else:
+#         print ("Salah pilih!")
+
+# while (True):
+#     show_menu()
